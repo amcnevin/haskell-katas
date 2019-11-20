266c45b0fb34f04ae3dccbce8120cf493d9836b0
amcnevin <amcnevin@gmail.com>
2019-11-19 22:25:05 -0600

    Exercise 4 completed

--- test/Ex04_ListsTuplesSpec.hs
+++ test/Ex04_ListsTuplesSpec.hs
@@ -11,17 +11,22 @@ spec :: Spec
 spec = do
   describe "tuples" $ do
     it "can select tuple's first value" $ do
-      pending
-      0 `shouldBe` 1
+      fst (1, 0) `shouldBe` 1
     it "can select tuple's second value" $ do
-      pending
-      True `shouldBe` False
+      snd (True,False) `shouldBe` False
     it "zip can produce tuple pairs" $ do
-      pending
-      [(0, 1)] `shouldBe` [(1, 4), (2, 5), (3, 6)]
-      [(1, "two")] `shouldBe` [(1, "one"), (2, "two"), (3, "three")]
-      [(1, "orange")] `shouldBe` [(1, "apple"), (2, "orange"), (3, "cherry")]
+      zip [1,2,3] [4,5,6] `shouldBe` [(1, 4), (2, 5), (3, 6)]
+      zip [1,2,3] ["one", "two", "three"] `shouldBe` [(1, "one"), (2, "two"), (3, "three")]
+      zip [1,2,3] [ "apple", "orange", "cherry"] `shouldBe` [(1, "apple"), (2, "orange"), (3, "cherry")]
     it "can calculate right triangle that has the perimeter of 24" $ do
-      pending
-      let triangles = []
+      -- a^2 + b^2 = c^2
+      -- P = a + b + c
+      let triangles =
+                 [ (a, b, c)
+                 | c <- [1..24]
+                 , b <- [1..c]
+                 , a <- [1..b]
+                 , a^2 + b^2 == c^2
+                 , a + b + c == 24
+                 ]
       triangles `shouldBe` [(6, 8, 10)]
